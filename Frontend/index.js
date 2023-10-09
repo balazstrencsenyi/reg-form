aeee57d22b83af401073ca6c3f38095feffbfcbb
balazstrencsenyi <b.trencsenyi@gmail.com>
2023-10-09 12:15:31 +0200

    added js

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,8 +1,24 @@
-const root = document.getElementById("root");
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("registration-form");
+  const message = document.getElementById("message");
 
-function createForm() {
-  const form = document.createElement("form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-  const input = document.createElement("input");
-  input.setAttribute("type", "text");
-}
+    const name = form.querySelector("#name").value;
+    const email = form.querySelector("#email").value;
+    const confirmEmail = form.querySelector("#confirm-email").value;
+    const source = form.querySelector("#source").value;
+    const agreement = form.querySelector("#agreement").checked;
+
+    if (!name || !email || !confirmEmail || !source || !agreement) {
+      message.innerHTML =
+        "Minden mezőt ki kell tölteni és az adatkezelési nyilatkozatot elfogadni!";
+    } else if (email !== confirmEmail) {
+      message.innerHTML = "Az email címek nem egyeznek!";
+    } else {
+      message.innerHTML = "Sikeres regisztráció!";
+      form.reset();
+    }
+  });
+});
