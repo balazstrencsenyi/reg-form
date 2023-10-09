6344ce8dffe68ba5a35b4634039e665bbb17839d
balazstrencsenyi <b.trencsenyi@gmail.com>
2023-10-09 12:15:14 +0200

    added css

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,1 +1,8 @@
 const root = document.getElementById("root");
+
+function createForm() {
+  const form = document.createElement("form");
+
+  const input = document.createElement("input");
+  input.setAttribute("type", "text");
+}
